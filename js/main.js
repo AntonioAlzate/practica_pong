81535753ee0408f8f512a2c260eea4a7caf8ed97
Antonio Alzate <antonioalzate7@gmail.com>
2021-11-19 01:40:25 -0500

    Se desarrollo el movimiento de las barras en pantalla y se realizo la creación de la pelota pero solo se muestra de manera visual.
    las barras poseen movimiento independiente, una con las flechas arriba y abajo y las otra con las letras W y S.

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 (function () {
-  self.Board = function (width, heigth) {
+  self.Board = function (width, height) {
     this.width = width;
-    this.heigth = heigth;
+    this.height = height;
     this.playing = false;
     this.gameOver = false;
     this.bars = [];
@@ -10,11 +10,24 @@
 
   self.Board.prototype = {
     get elementos() {
-      var elementos = this.bars;
+      var elementos = this.bars.map(function(bar) {return bar; });
       elementos.push(this.ball);
       return elementos;
-    }
-  }
+    },
+  };
+})();
+
+(function () {
+  self.Ball = function (x, y, radius, board) {
+    this.x = x;
+    this.y = y;
+    this.radius = radius;
+    this.speed_y = 0;
+    this.speed_x = 3;
+
+    board.ball = this;
+    this.kind = "circle";
+  };
 })();
 
 (function () {
@@ -33,67 +46,85 @@
 
   self.Bar.prototype = {
     down: function () {
-        this.y += this.speed;
+      this.y += this.speed;
     },
     up: function () {
-        this.y -= this.speed;
+      this.y -= this.speed;
     },
-    toString: function(){
-        return "x: " + this.x + " y: " + this.y;
-    }
-  }
+    toString: function () {
+      return "x: " + this.x + " y: " + this.y;
+    },
+  };
 })();
 
-(function() {
+(function () {
   self.BoardView = function (canvas, board) {
     this.canvas = canvas;
     this.canvas.width = board.width;
-    this.canvas.heigth = board.heigth;
+    this.canvas.height = board.height;
     this.board = board;
     this.ctx = canvas.getContext("2d");
   };
 
   self.BoardView.prototype = {
-      draw: function(){
-          for(var i = this.board.elementos.length -1; i>=0; i--){
-            var el = this.board.elementos[i];
-            
-            draw(this.ctx, el);
-          };
+    clean: function () {
+      this.ctx.clearRect(0, 0, this.board.width, this.board.height);
+    },
+    draw: function () {
+      for (var i = this.board.elementos.length - 1; i >= 0; i--) {
+        var el = this.board.elementos[i];
+
+        draw(this.ctx, el);
       }
+    },
+    play: function () {
+      this.clean();
+      this.draw();
+    },
   };
 
   function draw(ctx, elemento) {
-    if (elemento !== null && elemento.hasOwnProperty("kind")) {
-      switch (elemento.kind) {
-        case "rectangle":
-          ctx.fillRect(elemento.x, elemento.y, elemento.width, elemento.height);
-          break;
-      }
+    switch (elemento.kind) {
+      case "rectangle":
+        ctx.fillRect(elemento.x, elemento.y, elemento.width, elemento.height);
+        break;
+      case "circle":
+        ctx.beginPath();
+        ctx.arc(elemento.x, elemento.y, elemento.radius, 0, 7);
+        ctx.fill();
+        ctx.closePath();
+        break;
     }
-  };
+  }
 })();
 
 var board = new Board(600, 400);
-var bar = new Bar(20,50,15,50, board);
-var bar = new Bar(560,50,15,50, board);
-var canvas = document.getElementById('canvas');
+var bar = new Bar(20, 50, 15, 50, board);
+var bar2 = new Bar(560, 50, 15, 50, board);
+var canvas = document.getElementById("canvas");
 var boardView = new BoardView(canvas, board);
+var ball = new Ball(350,100, 10, board);
 
-document.addEventListener("keydown", function(ev) {
-    if(ev.keyCode == 38){
-        bar.up();
-    }
-    else if(ev.keyCode == 40){
-        bar.down();
-    }
+document.addEventListener("keydown", function (ev) {
+  ev.preventDefault();
+  if (ev.keyCode == 38) {
+    bar.up();
+  } else if (ev.keyCode == 40) {
+    bar.down();
+  } else if (ev.keyCode == 87) {
+    bar2.up();
+  } else if (ev.keyCode == 83) {
+    bar2.down();
+  }
 
-    console.log(bar.toString());
+  console.log(bar.toString());
 });
 
-self.addEventListener("load", main);
+//self.addEventListener("load", main);
 
-function main() {
+window.requestAnimationFrame(controller);
 
-  boardView.draw();
+function controller() {
+  boardView.play();
+  window.requestAnimationFrame(controller);
 }
